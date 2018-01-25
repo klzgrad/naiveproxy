9e4602b61c76945961766aa5351c6f941dd5f59b
klzgrad <kizdiv@gmail.com>
2018-01-25 05:02:13 -0500

    net, url: Remove icu

---
+++ src/url/url_canon_icu_alternatives_dummy.cc
@@ -0,0 +1,15 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/notreached.h"
+#include "url/url_canon.h"
+
+namespace url {
+
+bool IDNToASCII(std::u16string_view src, CanonOutputW* output) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+}  // namespace url
