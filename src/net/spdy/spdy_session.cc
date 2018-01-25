7bb8f48f9ee2c951f096bc54db1767d6c2727977
klzgrad <kizdiv@gmail.com>
2018-01-24 23:50:06 -0500

    net/spdy: Reduce warnings about RST on invalid streams
    Per RFC 7540#6.4:
    
      However, after sending the RST_STREAM, the sending endpoint MUST be
      prepared to receive and process additional frames sent on the stream
      that might have been sent by the peer prior to the arrival of the
      RST_STREAM.

--- src/net/spdy/spdy_session.cc
+++ src/net/spdy/spdy_session.cc
@@ -2726,7 +2726,7 @@ void SpdySession::OnRstStream(spdy::SpdyStreamId stream_id,
   auto it = active_streams_.find(stream_id);
   if (it == active_streams_.end()) {
     // NOTE:  it may just be that the stream was cancelled.
-    LOG(WARNING) << "Received RST for invalid stream" << stream_id;
+    VLOG(1) << "Received RST for invalid stream" << stream_id;
     return;
   }
 
@@ -2949,7 +2949,7 @@ void SpdySession::OnWindowUpdate(spdy::SpdyStreamId stream_id,
 
     if (it == active_streams_.end()) {
       // NOTE:  it may just be that the stream was cancelled.
-      LOG(WARNING) << "Received WINDOW_UPDATE for invalid stream " << stream_id;
+      VLOG(1) << "Received WINDOW_UPDATE for invalid stream " << stream_id;
       return;
     }
 
@@ -2994,7 +2994,7 @@ void SpdySession::OnHeaders(spdy::SpdyStreamId stream_id,
   auto it = active_streams_.find(stream_id);
   if (it == active_streams_.end()) {
     // NOTE:  it may just be that the stream was cancelled.
-    LOG(WARNING) << "Received HEADERS for invalid stream " << stream_id;
+    VLOG(1) << "Received HEADERS for invalid stream " << stream_id;
     return;
   }
 
