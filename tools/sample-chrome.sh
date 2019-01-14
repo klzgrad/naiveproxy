d622fd5d18df02d2feebc812745e0b2fb0c40160
klzgrad <kizdiv@gmail.com>
2019-01-14 00:42:27 -0500

    Add source import tool

---
+++ tools/sample-chrome.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+if [ ! "$1" ]; then
+  echo "Usage: $0 DOMAIN"
+  exit 1
+fi
+
+domain="$1"
+sudo echo
+
+tempdir=$(mktemp -d)
+rm -f /tmp/keys
+sudo rm -f /tmp/chrome.pcapng
+
+sudo tshark -Q -a duration:10 -w /tmp/chrome.pcapng &
+tsharkpid=$!
+sleep 1
+
+google-chrome --user-data-dir="$tempdir" --ssl-key-log-file=/tmp/keys --no-default-browser-check --no-first-run --disable-quic "https://$domain/" &
+chromepid=$!
+
+sleep 10
+kill $chromepid
+rm -rf "$tempdir"
+
+wait $tsharkpid
+sudo chmod +r /tmp/chrome.pcapng
+
+./parse-pcap-stream.py /tmp/chrome.pcapng "$domain"
