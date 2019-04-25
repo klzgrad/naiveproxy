290018a615d12626bcc23c28b60a53c4511a180d
klzgrad <kizdiv@gmail.com>
2019-04-26 01:40:46 +0800

    Add continuous integration and tests

---
+++ tests/basic.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -ex
+
+script_dir=$(dirname "$PWD/$0")
+
+[ "$1" ] || exit 1
+naive="$PWD/$1"
+
+. ./get-sysroot.sh
+
+if [ "$WITH_ANDROID_IMG" ]; then
+  rootfs="$PWD/out/sysroot-build/android/$WITH_ANDROID_IMG"
+elif [ "$WITH_SYSROOT" ]; then
+  rootfs="$PWD/$WITH_SYSROOT"
+fi
+
+cd /tmp
+python3 "$script_dir"/basic.py --naive="$naive" --rootfs="$rootfs" --target_cpu="$target_cpu"
