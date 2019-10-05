53c10dc0ae751f9bf5a1584f375ad2d1f9d84f2e
klzgrad <kizdiv@gmail.com>
2019-10-05 15:52:34 +0800

    Add sysroot script

---
+++ src/build/linux/sysroot_scripts/sysroot-creator-sid-naive.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+.() { :; }
+
+source "${SCRIPT_DIR}/sysroot-creator-sid.sh"
+
+unset -f .
+
+DEBIAN_PACKAGES='
+  libc6
+  libc6-dev
+  linux-libc-dev
+  libgcc-6-dev
+  libgcc1
+  libgomp1
+  libatomic1
+  libstdc++6
+  libnspr4
+  libnspr4-dev
+  libnss3
+  libnss3-dev
+  libsqlite3-0
+'
+
+DEBIAN_PACKAGES_AMD64='
+  liblsan0
+  libtsan0
+'
+DEBIAN_PACKAGES_X86='
+  libasan3
+  libcilkrts5
+  libitm1
+  libmpx2
+  libquadmath0
+  libubsan0
+'
+DEBIAN_PACKAGES_ARM='
+  libasan3
+  libubsan0
+'
+DEBIAN_PACKAGES_ARM64='
+  libasan3
+  libitm1
+  libubsan0
+'
+
+# Disables libdbus workarounds
+ln -sf /bin/true strip
+ln -sf /bin/true arm-linux-gnueabihf-strip
+export PATH="$PWD:$PATH"
+cp() {
+  [ "${1##*/}" = libdbus-1-3-symbols ] && return
+  /bin/cp "$@"
+}
+
+trap "cd $PWD; rm strip arm-linux-gnueabihf-strip" EXIT
+
+. "${SCRIPT_DIR}/sysroot-creator.sh"
