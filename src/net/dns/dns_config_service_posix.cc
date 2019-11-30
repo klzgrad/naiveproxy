b465252d132d38948334c84d06844773b1742a79
klzgrad <kizdiv@gmail.com>
2019-11-30 22:21:18 +0800

    dns: Support Musl

--- src/net/dns/dns_config_service_posix.cc
+++ src/net/dns/dns_config_service_posix.cc
@@ -151,7 +151,7 @@ ConfigParsePosixResult ReadDnsConfig(DnsConfig* dns_config) {
 #if !defined(OS_ANDROID)
   ConfigParsePosixResult result;
 // TODO(fuchsia): Use res_ninit() when it's implemented on Fuchsia.
-#if defined(OS_OPENBSD) || defined(OS_FUCHSIA)
+#if defined(OS_OPENBSD) || defined(OS_FUCHSIA) || defined(__UCLIBC__)
   // Note: res_ninit in glibc always returns 0 and sets RES_INIT.
   // res_init behaves the same way.
   memset(&_res, 0, sizeof(_res));
