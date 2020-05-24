e9cd445c8af28ee86222c3879e41340f6ef6e16a
klzgrad <kizdiv@gmail.com>
2020-05-25 00:30:52 +0800

    Negotiate padding capability automatically
    Client: On the first connection does a full Open and detects if the
    server supports padding by checking for "Padding" header in the
    response. Applies padding if the server does. In the following
    connections it's back to Fast Open.
    
    Server: Detects if the client supports padding by checking for "Padding"
    header in the CONNECT request. Applies padding if the client does.
    
    Both client and server always send "Padding" headers to somewhat protect
    the request and response headers' packet lengths, even if the other side
    may not acknowledge padding negotiation, either due to old version or
    "Padding" headers being dropped by the frontend.
    
    The manual option --padding is removed.

--- src/net/tools/naive/naive_proxy.h
+++ src/net/tools/naive/naive_proxy.h
@@ -18,6 +18,7 @@
 #include "net/proxy_resolution/proxy_info.h"
 #include "net/ssl/ssl_config.h"
 #include "net/tools/naive/naive_connection.h"
+#include "net/tools/naive/naive_protocol.h"
 
 namespace net {
 
@@ -32,8 +33,7 @@ class RedirectResolver;
 class NaiveProxy {
  public:
   NaiveProxy(std::unique_ptr<ServerSocket> server_socket,
-             NaiveConnection::Protocol protocol,
-             bool use_padding,
+             ClientProtocol protocol,
              int concurrency,
              RedirectResolver* resolver,
              HttpNetworkSession* session,
@@ -58,8 +58,7 @@ class NaiveProxy {
   NaiveConnection* FindConnection(unsigned int connection_id);
 
   std::unique_ptr<ServerSocket> listen_socket_;
-  NaiveConnection::Protocol protocol_;
-  bool use_padding_;
+  ClientProtocol protocol_;
   int concurrency_;
   ProxyInfo proxy_info_;
   SSLConfig server_ssl_config_;
