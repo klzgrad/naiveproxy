e6f0682fcc0e05573ec15940fd9731299e81612b
klzgrad <kizdiv@gmail.com>
2021-05-16 00:46:34 +0800

    cert: Use builtin verifier on Android and Linux

--- src/net/cert/cert_verifier.cc
+++ src/net/cert/cert_verifier.cc
@@ -87,7 +87,8 @@ std::unique_ptr<CertVerifier> CertVerifier::CreateDefaultWithoutCaching(
   }
 #endif
   if (!verify_proc) {
-#if BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || \
+    BUILDFLAG(IS_ANDROID)
     verify_proc =
         CertVerifyProc::CreateBuiltinVerifyProc(std::move(cert_net_fetcher));
 #else
