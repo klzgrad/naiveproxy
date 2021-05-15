66a3be9684d725206badade0000a9c6914e27f5c
klzgrad <kizdiv@gmail.com>
2021-05-16 00:47:27 +0800

    cert: Handle AIA response in PKCS#7 format

--- src/net/cert/internal/cert_issuer_source_aia.cc
+++ src/net/cert/internal/cert_issuer_source_aia.cc
@@ -9,6 +9,7 @@
 #include "net/cert/cert_net_fetcher.h"
 #include "net/cert/internal/cert_errors.h"
 #include "net/cert/pem.h"
+#include "net/cert/x509_certificate.h"
 #include "net/cert/x509_util.h"
 #include "url/gurl.h"
 
@@ -112,6 +113,23 @@ bool AiaRequest::AddCompletedFetchToResults(Error error,
   //    certificates and SHOULD be able to accept "certs-only" CMS messages.
   //
   // TODO(mattm): Is supporting CMS message format important?
+
+  // Handles PKCS#7 encoded certificates
+  CertificateList certs = X509Certificate::CreateCertificateListFromBytes(
+      reinterpret_cast<const char*>(fetched_bytes.data()), fetched_bytes.size(),
+      X509Certificate::FORMAT_AUTO);
+  bool certs_ok = false;
+  for (const auto& cert : certs) {
+    auto parsed = ParsedCertificate::Create(
+        bssl::UpRef(cert->cert_buffer()),
+        x509_util::DefaultParseCertificateOptions(), /*errors=*/nullptr);
+    if (parsed) {
+      results->push_back(parsed);
+      certs_ok = true;
+    }
+  }
+  if (certs_ok)
+    return true;
 
   // TODO(https://crbug.com/870359): Some AIA responses are served as PEM, which
   // is not part of RFC 5280's profile.
