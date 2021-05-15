cbdc164ebae4abb872ef7f1a3c544eab70ac4f2a
klzgrad <kizdiv@gmail.com>
2021-05-16 00:46:34 +0800

    cert: Use builtin verifier on Android and Linux

--- src/net/cert/cert_verifier.cc
+++ src/net/cert/cert_verifier.cc
@@ -40,7 +40,8 @@ class DefaultCertVerifyProcFactory : public net::CertVerifyProcFactory {
     return CertVerifyProc::CreateBuiltinWithChromeRootStore(
         std::move(cert_net_fetcher), impl_params.crl_set,
         base::OptionalToPtr(impl_params.root_store_data));
-#elif BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#elif BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
     return CertVerifyProc::CreateBuiltinVerifyProc(std::move(cert_net_fetcher),
                                                    impl_params.crl_set);
 #else
