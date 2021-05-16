4461baa1e1d1e9ac75263a55bc7107f65c5b5c7c
klzgrad <kizdiv@gmail.com>
2021-05-16 10:10:09 +0800

    net: Add Android stubs

---
+++ src/net/android/network_library_stub.cc
@@ -0,0 +1,76 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/android/network_library.h"
+
+namespace net {
+namespace android {
+
+void VerifyX509CertChain(const std::vector<std::string>& cert_chain,
+                         base::StringPiece auth_type,
+                         base::StringPiece host,
+                         CertVerifyStatusAndroid* status,
+                         bool* is_issued_by_known_root,
+                         std::vector<std::string>* verified_chain) {
+}
+
+void AddTestRootCertificate(const uint8_t* cert, size_t len) {
+}
+
+void ClearTestRootCertificates() {
+}
+
+bool IsCleartextPermitted(const std::string& host) {
+  return true;
+}
+
+bool HaveOnlyLoopbackAddresses() {
+  return false;
+}
+
+bool GetMimeTypeFromExtension(const std::string& extension,
+                              std::string* result) {
+  return false;
+}
+
+std::string GetTelephonyNetworkOperator() {
+  return {};
+}
+
+std::string GetTelephonySimOperator() {
+  return {};
+}
+
+bool GetIsRoaming() {
+  return false;
+}
+
+bool GetIsCaptivePortal() {
+  return false;
+}
+
+std::string GetWifiSSID() {
+  return {};
+}
+
+base::Optional<int32_t> GetWifiSignalLevel() {
+  return {};
+}
+
+bool GetDnsServers(std::vector<IPEndPoint>* dns_servers,
+                   bool* dns_over_tls_active,
+                   std::string* dns_over_tls_hostname,
+                   std::vector<std::string>* search_suffixes) {
+  return false;
+}
+
+bool ReportBadDefaultNetwork() {
+  return false;
+}
+
+void TagSocket(SocketDescriptor socket, uid_t uid, int32_t tag) {
+}
+
+}  // namespace android
+}  // namespace net
