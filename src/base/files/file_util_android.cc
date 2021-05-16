5b95ee4e70d9c3ee8d5385af47dd44c664b6b8ff
klzgrad <kizdiv@gmail.com>
2021-05-16 10:10:30 +0800

    android: base: Add Android stubs

--- src/base/files/file_util_android.cc
+++ src/base/files/file_util_android.cc
@@ -4,22 +4,10 @@
 
 #include "base/files/file_util.h"
 
-#include "base/android/jni_string.h"
 #include "base/files/file_path.h"
 #include "base/path_service.h"
 
-// Must come after all headers that specialize FromJniType() / ToJniType().
-#include "base/base_jni/FileUtils_jni.h"
-
 namespace base {
-namespace android {
-
-static std::string JNI_FileUtils_GetAbsoluteFilePath(JNIEnv* env,
-                                                     std::string& file_path) {
-  return MakeAbsoluteFilePath(base::FilePath(file_path)).value();
-}
-
-}  // namespace android
 
 bool GetShmemTempDir(bool executable, base::FilePath* path) {
   return PathService::Get(base::DIR_CACHE, path);
