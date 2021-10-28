0aa27e8bd37428f2124a891be1e5e793928cd726
klzgrad <kizdiv@gmail.com>
2021-10-28 22:15:29 +0800

    Revert "allocator: Disable uffd support for Musl"
    No longer needed with use_partition_alloc=false on OpenWrt Musl.
    
    This reverts commit b82b1a7cc7e9800e77f22e0bf7f8222793f84d7d.

--- src/base/allocator/partition_allocator/partition_alloc_config.h
+++ src/base/allocator/partition_allocator/partition_alloc_config.h
@@ -35,7 +35,7 @@ static_assert(sizeof(void*) != 8, "");
 #endif
 
 #if defined(PA_HAS_64_BITS_POINTERS) && \
-    (defined(OS_LINUX) || defined(OS_ANDROID)) && !defined(__MUSL__)
+    (defined(OS_LINUX) || defined(OS_ANDROID))
 #include <linux/version.h>
 // TODO(bikineev): Enable for ChromeOS.
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 8, 0)
