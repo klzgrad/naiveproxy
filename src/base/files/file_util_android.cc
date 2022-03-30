99d1c10ad1f82839ea39b6a94bf3827d44658fd9
klzgrad <kizdiv@gmail.com>
2022-03-30 20:23:40 +0800

    base: Remove JNI function on Android

--- src/base/files/file_util_android.cc
+++ src/base/files/file_util_android.cc
@@ -4,26 +4,10 @@
 
 #include "base/files/file_util.h"
 
-#include "base/android/jni_android.h"
-#include "base/android/jni_string.h"
-#include "base/base_jni/FileUtils_jni.h"
 #include "base/files/file_path.h"
 #include "base/path_service.h"
 
-using base::android::JavaParamRef;
-using base::android::JavaRef;
-using base::android::ScopedJavaLocalRef;
-
 namespace base {
-namespace android {
-
-static base::FilePath JNI_FileUtils_GetAbsoluteFilePath(
-    JNIEnv* env,
-    base::FilePath& file_path) {
-  return MakeAbsoluteFilePath(file_path);
-}
-
-}  // namespace android
 
 bool GetShmemTempDir(bool executable, base::FilePath* path) {
   return PathService::Get(base::DIR_CACHE, path);
