e45001c0d0555546591a384a9721e403e0d953b4
klzgrad <kizdiv@gmail.com>
2022-05-04 01:41:30 +0800

    tools: Support import from any branch

--- tools/import-upstream.sh
+++ tools/import-upstream.sh
@@ -9,11 +9,12 @@ name="chromium-$want_version"
 tarball="$name.tar.xz"
 url="https://commondatastorage.googleapis.com/chromium-browser-official/$tarball"
 root=$(git rev-list --max-parents=0 HEAD)
+branch=$(git branch --show-current)
 git config core.autocrlf false
 git config core.safecrlf false
 git -c advice.detachedHead=false checkout $root
 rm -rf src
-git checkout master -- tools
+git checkout "$branch" -- tools
 sed -i "s/^\^/$name\//" tools/include.txt
 if [ -f "/tmp/$tarball" ]; then
   cat "/tmp/$tarball" | tar xJf - --wildcards --wildcards-match-slash -T tools/include.txt -X tools/exclude.txt
@@ -24,4 +25,4 @@ mv "$name" src
 git rm --quiet --force -r tools
 git add src
 git commit --quiet --amend -m "Import $name" --date=now
-git rebase --onto HEAD "$root" master
+git rebase --onto HEAD "$root" "$branch"
