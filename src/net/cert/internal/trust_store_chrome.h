b9a8118e5fa187d50d2d7a51facaf6d78606c27d
klzgrad <kizdiv@gmail.com>
2023-06-04 11:54:34 +0800

    net/cert: Fix iwyu

--- src/net/cert/internal/trust_store_chrome.h
+++ src/net/cert/internal/trust_store_chrome.h
@@ -14,9 +14,13 @@
 #include "base/version.h"
 #include "crypto/sha2.h"
 #include "net/base/net_export.h"
-#include "net/cert/root_store_proto_lite/root_store.pb.h"
 #include "third_party/boringssl/src/pki/trust_store.h"
 #include "third_party/boringssl/src/pki/trust_store_in_memory.h"
+
+// Forward declares this so root_store.pb.h would not be a public dependency.
+namespace chrome_root_store {
+class RootStore;
+}
 
 namespace net {
 
