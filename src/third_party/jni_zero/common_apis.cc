f962af64886ea16fd98cd5c12ebf494bba1189b4
klzgrad <kizdiv@gmail.com>
2024-07-27 10:18:11 +0800

    third_party/jni_zero: Remove JNI functions

--- src/third_party/jni_zero/common_apis.cc
+++ src/third_party/jni_zero/common_apis.cc
@@ -4,20 +4,17 @@
 
 #include "third_party/jni_zero/common_apis.h"
 
-#include "third_party/jni_zero/system_jni/Arrays_jni.h"
-#include "third_party/jni_zero/system_jni/Collection_jni.h"
-
 namespace jni_zero {
 
 ScopedJavaLocalRef<jobjectArray> CollectionToArray(
     JNIEnv* env,
     const JavaRef<jobject>& collection) {
-  return JNI_Collection::Java_Collection_toArray(env, collection);
+  return nullptr;
 }
 
 ScopedJavaLocalRef<jobject> ArrayToList(JNIEnv* env,
                                         const JavaRef<jobjectArray>& array) {
-  return JNI_Arrays::Java_Arrays_asList(env, array);
+  return nullptr;
 }
 
 }  // namespace jni_zero
