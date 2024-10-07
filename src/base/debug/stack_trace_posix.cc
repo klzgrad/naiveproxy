00ee2eb7a6e9fb4e3fe8773dafeb4425cad9fc30
klzgrad <kizdiv@gmail.com>
2024-10-07 14:46:36 +0800

    musl: base: Fix stack trace printing

--- src/base/debug/stack_trace_posix.cc
+++ src/base/debug/stack_trace_posix.cc
@@ -1083,6 +1083,10 @@ void StackTrace::OutputToStreamWithPrefixImpl(
   StreamBacktraceOutputHandler handler(os);
   ProcessBacktrace(addresses(), prefix_string, &handler);
 }
+#else
+void StackTrace::OutputToStreamWithPrefixImpl(
+    std::ostream*, cstring_view) const {
+}
 #endif
 
 namespace internal {
