12f08d50ceb52c92eefbe4c248bded34d7e17d4a
klzgrad <kizdiv@gmail.com>
2025-01-14 19:29:28 +0800

    base: Remove Rust

--- src/base/logging.cc
+++ src/base/logging.cc
@@ -123,7 +123,7 @@ typedef FILE* FileHandle;
 #include "base/fuchsia/scoped_fx_logger.h"
 #endif
 
-#if !BUILDFLAG(IS_NACL)
+#if 0  // Disables Rust
 #include "base/logging/rust_logger.rs.h"
 #endif
 
@@ -534,7 +534,7 @@ bool BaseInitLoggingImpl(const LoggingSettings& settings) {
   }
 #endif
 
-#if !BUILDFLAG(IS_NACL)
+#if 0  // Disables Rust
   // Connects Rust logging with the //base logging functionality.
   internal::init_rust_log_crate();
 #endif
