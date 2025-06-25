2a0ddfa840b3ec1ad6172d04ba8148750cfb7d41
klzgrad <kizdiv@gmail.com>
2025-06-25 19:11:55 +0800

    base: Remove Rust

--- src/base/json/json_reader.cc
+++ src/base/json/json_reader.cc
@@ -7,23 +7,16 @@
 #include <string_view>
 #include <utility>
 
+#include "base/json/json_parser.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "build/build_config.h"
 
-#if BUILDFLAG(IS_NACL)
-#include "base/json/json_parser.h"
-#else
-#include "base/strings/string_view_rust.h"
-#include "third_party/rust/serde_json_lenient/v0_2/wrapper/functions.h"
-#include "third_party/rust/serde_json_lenient/v0_2/wrapper/lib.rs.h"
-#endif
-
 // TODO(crbug.com/40811643): Move the C++ parser into components/nacl to just
 // run in-process there. Don't compile base::JSONReader on NaCL at all.
-#if !BUILDFLAG(IS_NACL)
+#if 0
 
 namespace {
 const char kSecurityJsonParsingTime[] = "Security.JSONParser.ParsingTime";
@@ -144,7 +137,7 @@ std::string JSONReader::Error::ToString() const {
 std::optional<Value> JSONReader::Read(std::string_view json,
                                       int options,
                                       size_t max_depth) {
-#if BUILDFLAG(IS_NACL)
+#if 1
   internal::JSONParser parser(options, max_depth);
   return parser.Parse(json);
 #else   // BUILDFLAG(IS_NACL)
@@ -185,7 +178,7 @@ std::optional<Value::List> JSONReader::ReadList(std::string_view json,
 JSONReader::Result JSONReader::ReadAndReturnValueWithError(
     std::string_view json,
     int options) {
-#if BUILDFLAG(IS_NACL)
+#if 1
   internal::JSONParser parser(options);
   auto value = parser.Parse(json);
   if (!value) {
