64c969277106de290d985f2656d50420f8505798
klzgrad <kizdiv@gmail.com>
2026-09-12 14:02:46 +0800

    Lower Windows SDK requirement
    until github installs newer windows sdk

--- src/build/toolchain/win/setup_toolchain.py
+++ src/build/toolchain/win/setup_toolchain.py
@@ -22,7 +22,7 @@ import action_helpers
 import gn_helpers
 
 SCRIPT_DIR = os.path.dirname(__file__)
-SDK_VERSION = '10.0.28000.0'
+SDK_VERSION = '10.0.26100.0'
 MSVC_DIR = re.compile('^.*/VC/Tools/MSVC/[^/]+/include$')
 WINDOWS_KITS_DIR = re.compile(r'^(.*/Windows Kits/\d+/Include/[^/]+)/.*')
 
--- src/build/vs_toolchain.py
+++ src/build/vs_toolchain.py
@@ -59,8 +59,8 @@ from gn_helpers import ToGNString
 # * docs/windows_build_instructions.md
 #   Make sure any version numbers in the documentation match the code.
 #
-TOOLCHAIN_HASH = '3bfcb536c8'
-SDK_VERSION = '10.0.28000.0'
+TOOLCHAIN_HASH = 'e66617bc68'
+SDK_VERSION = '10.0.26100.0'
 
 # Visual Studio versions are listed in descending order of priority.
 # The first version is assumed by this script to be the one that is packaged,
